a32acaac6c454c85a1416dcf087835551ca032fd
etsVain <535654314@qq.com>
2018-11-07 17:33:41 +0800

    1.修改命名 2.禁止右键事件 3.增加拖动边界

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-import SeeImageComponent from './SeeImage'
+import SeeImageComponent from './ImageViewer'
+
 const SeeImage = {}
 
 SeeImage.install = Vue => {
@@ -15,15 +16,37 @@ SeeImage.install = Vue => {
             instance.isShowImageDialog = true;
             let oDiv = el;   //当前元素
             oDiv.onmousedown = function (e) {
+                if (e.button === 2) return;
+
                 e.preventDefault();
+
+                let bw = document.body.clientWidth;
+                let bh = document.body.clientHeight;
                 //鼠标按下，计算当前元素距离可视区的距离
                 let disX = e.clientX - oDiv.offsetLeft;
                 let disY = e.clientY - oDiv.offsetTop;
 
                 document.onmousemove = function (e) {
                     //通过事件委托，计算移动的距离
-                    let l = e.clientX - disX;
-                    let t = e.clientY - disY;
+                    let l = 0, t = 0;
+                    if (e.clientX >= bw) {
+                        l = bw - disX;
+                    } else if (e.clientX <= 0) {
+                        {
+                            l =0- disX;
+                        }
+                    } else {
+                        l = e.clientX - disX;
+                    }
+                    if (e.clientY >= bh) {
+                        t = bh - disY;
+                    }else if(e.clientY <= 0) {
+                        t =0- disY;
+                    }
+                    else {
+                        t = e.clientY - disY;
+                    }
+
                     //移动当前元素
                     oDiv.style.left = l + 'px';
                     oDiv.style.top = t + 'px';
